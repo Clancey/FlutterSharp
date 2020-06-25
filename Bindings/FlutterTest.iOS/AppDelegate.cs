11255df999a07bd1ee84e3cb6f97f484b05ba09e
James Clancey <james.clancey@gmail.com>
2020-06-25 11:30:15 -0800

    Removed duplicate files. Build.sh builds native bits now
    Fixed iOS build

--- Bindings/FlutterTest.iOS/AppDelegate.cs
+++ Bindings/FlutterTest.iOS/AppDelegate.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Flutter;
-using Flutter.iOS;
 using Foundation;
 using UIKit;
 
--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd flutter_module
 flutter build ios-framework --output=build
+flutter build aar
