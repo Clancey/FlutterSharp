87518474ff85085acf1c876d7701dbfc7a79793a
James Clancey <james.clancey@gmail.com>
2020-06-30 22:05:55 -0800

    Cleaned up build script

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 cd flutter_module
-rm -rf .ios
+flutter clean
 flutter build ios-framework -v --output=build
 flutter build aar
-
-dotnet run --project=utils/SkiaSharpGenerator/SkiaSharpGenerator.csproj -- generate --config /Users/clancey/Projects/SkiaSharp/binding/libSkiaSharp.json --skia /Users/clancey/Projects/FlutterBridge/flutter_sharp/ios  --output flutter..generated.cs  
-
-dotnet run --project=utils/SkiaSharpGenerator/SkiaSharpGenerator.csproj -- generate --config binding/libSkiaSharp.json --skia externals/skia --output binding/Binding/SkiaApi.generated.cs
