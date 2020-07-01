1b9fb9019cf0d38137f30a073bf128fa9882f002
James Clancey <james.clancey@gmail.com>
2020-07-01 01:26:32 -0800

    added fields to prevent GC

--- Bindings/Flutter/Wigets/AppBar.cs
+++ Bindings/Flutter/Wigets/AppBar.cs
@@ -6,11 +6,13 @@ namespace Flutter {
 
 	public class AppBar : Widget {
 		protected override FlutterObjectStruct CreateBackingStruct() => new AppBarStruct();
+		Widget title;
+		Widget bottom;
 		public AppBar(Widget title = null, Widget bottom = null)
 		{
 			var backingStruct = GetBackingStruct<AppBarStruct>();
-			backingStruct.Title = title;
-			backingStruct.Bottom = bottom;
+			backingStruct.Title = this.title =  title;
+			backingStruct.Bottom = this.bottom = bottom;
 		}
 	}
 }
--- Bindings/Flutter/Wigets/Scaffold.cs
+++ Bindings/Flutter/Wigets/Scaffold.cs
@@ -3,13 +3,17 @@ using System;
 namespace Flutter {
 	public class Scaffold : Widget {
 
+		AppBar appbar;
+		FloatingActionButton floatingActionButton;
+		Drawer drawer;
+		Widget body;
 		public Scaffold(AppBar appbar = null, FloatingActionButton floatingActionButton = null, Drawer drawer = null, Widget body = null)
 		{
 			var s = GetBackingStruct<ScaffoldStruct>();
-			s.AppBar = appbar;
-			s.FloatingActionButton = floatingActionButton;
-			s.Drawer = drawer;
-			s.Body = body;
+			s.AppBar = this.appbar =appbar;
+			s.FloatingActionButton = this.floatingActionButton = floatingActionButton;
+			s.Drawer = this.drawer = drawer;
+			s.Body = this.body = body;
 		}
 
 		protected override FlutterObjectStruct CreateBackingStruct() => new ScaffoldStruct();
