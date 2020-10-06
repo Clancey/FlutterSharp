b81dd26d052eaefaf565d044095866503187e8c2
James Clancey <james.clancey@gmail.com>
2020-10-06 15:06:42 -0800

    Force building the flutter module in debug mode

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd flutter_module
 flutter clean
-flutter build ios-framework --output=build
-flutter build aar
+flutter build ios-framework --output=build --debug
+flutter build aar --debug
