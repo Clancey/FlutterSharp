56858d75c561f38ab109d5e44aedf0e75b7ee104
James Clancey <james.clancey@gmail.com>
2021-08-20 16:16:56 -0800

    fixed build order

--- build.sh
+++ build.sh
@@ -4,8 +4,6 @@ flutter_embedding_debug="https://storage.googleapis.com/download.flutter.io/io/f
 
 
 cd flutter_module
-echo "Downloading Flutter embedding debug"
-curl -o  build/flutter_embedding_debug.jar $flutter_embedding_debug
 echo "Cleaning Flutter"
 flutter clean
 echo "Building Flutter iOS"
@@ -14,3 +12,6 @@ echo "Building Flutter Android AAR"
 flutter build aar --debug
 echo "Building Flutter Android APK"
 flutter build apk --debug
+
+echo "Downloading Flutter embedding debug"
+curl -o  build/flutter_embedding_debug.jar $flutter_embedding_debug
