24c1dda69f00b11e39008a740c4f69971f343fb4
James Clancey <james.clancey@gmail.com>
2021-08-24 16:30:46 -0800

    the project now uses the binding project

--- build.sh
+++ build.sh
@@ -10,14 +10,14 @@ flutter_embedding_debug="https://storage.googleapis.com/download.flutter.io/io/f
 
 cd flutter_module
 
-# echo "Cleaning Flutter"
-# flutter clean
-# echo "Building Flutter iOS"
-# flutter build ios-framework --output=build --debug
-# echo "Building Flutter Android AAR"
-# flutter build aar --debug
-# echo "Building Flutter Android APK"
-# flutter build apk --debug
+echo "Cleaning Flutter"
+flutter clean
+echo "Building Flutter iOS"
+flutter build ios-framework --output=build --debug
+echo "Building Flutter Android AAR"
+flutter build aar --debug
+echo "Building Flutter Android APK"
+flutter build apk --debug
 
 echo "Downloading Flutter embedding debug"
 curl -o  build/flutter_embedding_debug.jar $flutter_embedding_debug
